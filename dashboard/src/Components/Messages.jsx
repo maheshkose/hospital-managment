8b58148c7ee5480f5c593a76989121ea01e03d85
mahesh kose <ms2kose@gmail.com>
2025-08-07 13:42:14 +0530

    added url to message component

--- dashboard/src/Components/Messages.jsx
+++ dashboard/src/Components/Messages.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AppContext from "../../Context/AppContext";
+import { url } from "./url";
+url
 
 const Messages = () => {
   const navigate = useNavigate();
