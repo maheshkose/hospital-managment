29b2c2bfa70f749ef37ad5ad05f2441dec963c19
mahesh kose <ms2kose@gmail.com>
2025-08-08 15:25:32 +0530

    a mistake

--- dashboard/src/Components/url.js
+++ dashboard/src/Components/url.js
@@ -1,6 +1,6 @@
-// export const url ="https://hospital-managment-backend-knzo.onrender.com/api/v1";
+ export const url ="https://hospital-managment-backend-knzo.onrender.com/api/v1";
 
-export const url ="http://localhost:4000/api/v1"; // Local development URL
+//export const url ="http://localhost:4000/api/v1"; // Local development URL
 
 //http://localhost:4000/api/v1/user/doctor/addnew
 //https://hospital-managment-backend-knzo.onrender.com/api/v1/user/doctor/addnew
